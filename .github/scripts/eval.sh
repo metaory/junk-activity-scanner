380c4bdeadc881801a27d55416c90b30c6b18ac9
metaory <metaory@gmail.com>
2024-10-09 20:25:47 +0800

    fix eval path bug

--- .github/scripts/eval.sh
+++ .github/scripts/eval.sh
@@ -7,17 +7,17 @@ mapfile -t EVALS < <(find evals/* -type f -name 'config.yml' | sed 's/.config.ym
 function run_evals {
   local target="${1:?BAD_TARGET}"
   for evaluator in "${EVALS[@]}"; do
-    echo "> ${evaluator}"
+    echo "[E]> ${evaluator}"
+    echo "[T]> ${target}"
     read -r -d '\n' enabled cmd < <(yq '.enabled, .command + " " + .arguments' <"${evaluator}/config.yml")
     if [ "$enabled" == false ]; then
       echo "evaluator is disabled. -- skipping.."
     else
       echo "running evaluator.."
-      echo "$cmd"
-      {
-        cd "$evaluator" && echo "${target}" | $cmd
-        cd - || return
-      }
+      (
+        cd "$evaluator" || return
+        echo "${target}" | $cmd
+      )
     fi
   done
 }
--- lib/content.sh
+++ lib/content.sh
@@ -4,20 +4,20 @@ GH_API_VER='X-GitHub-Api-Version: 2022-11-28'
 
 FULL_NAME="${1:?MISS_REPO}"
 FULL_PATH="${2:?MISS_PATH}"
-FILE_PATH="/tmp/junk/${FULL_NAME}/${FULL_PATH}"
+FILE_PATH="${FULL_NAME}/${FULL_PATH}"
 
-mkdir -p "$(dirname $FILE_PATH)" &>/dev/null || :
+mkdir -p "$(dirname "$FILE_PATH")" &>/dev/null || :
 
 if [ -f "$FILE_PATH" ]; then
-  printf "$FILE_PATH"
+  echo -n "$FILE_PATH"
   exit 0
 fi
 
 gh api \
   -H "Accept: application/vnd.github.raw+object" \
   -H "$GH_API_VER" \
-  "/repos/${FULL_NAME}/contents/${FULL_PATH}" > "${FILE_PATH}" &>/dev/null || :
+  "/repos/${FULL_NAME}/contents/${FULL_PATH}" > "${FILE_PATH}" || :
 
-printf "$FILE_PATH"
+echo -n "$FILE_PATH"
 
 # vim: ft=bash
